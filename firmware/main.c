63c22da62f4000c03f12c9c40786f0923fd4f65e
namedummy568-stack <namedummy568@gmail.com>
2025-09-19 11:50:43 +0530

    feat: Add support for new sensor array

--- firmware/main.c
+++ firmware/main.c
@@ -2,10 +2,12 @@
 #include <secure_boot.h>
 #include <network_stack.h>
 #include <power_management.h>
+#include <sensor_array.h>
 
 void main() {
     init_secure_boot();
     patch_network_vulnerability();
     optimize_power_management();
+    init_sensor_array();
     // ... rest of firmware
 }
