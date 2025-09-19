ce15f5113de8a26a9aaccde7f430ae5d22401859
namedummy568-stack <namedummy568@gmail.com>
2025-09-19 11:50:41 +0530

    fix: Patch critical vulnerability in network stack

--- firmware/main.c
+++ firmware/main.c
@@ -1,7 +1,9 @@
 /* Initial secure boot implementation */
 #include <secure_boot.h>
+#include <network_stack.h>
 
 void main() {
     init_secure_boot();
+    patch_network_vulnerability();
     // ... rest of firmware
 }
