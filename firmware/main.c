025b2ab9c9270b05ec20fd463dd475d4ecfa4eaa
namedummy568-stack <namedummy568@gmail.com>
2025-09-19 11:50:42 +0530

    refactor: Optimize power management module

--- firmware/main.c
+++ firmware/main.c
@@ -1,9 +1,11 @@
 /* Initial secure boot implementation */
 #include <secure_boot.h>
 #include <network_stack.h>
+#include <power_management.h>
 
 void main() {
     init_secure_boot();
     patch_network_vulnerability();
+    optimize_power_management();
     // ... rest of firmware
 }
