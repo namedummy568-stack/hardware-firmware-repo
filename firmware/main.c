4796e6a6b06feb94ced07f1013c62c7b7e7f7b90
namedummy568-stack <namedummy568@gmail.com>
2025-09-19 11:50:44 +0530

    chore: Update build system dependencies

--- firmware/main.c
+++ firmware/main.c
@@ -3,11 +3,13 @@
 #include <network_stack.h>
 #include <power_management.h>
 #include <sensor_array.h>
+#include <build_deps.h>
 
 void main() {
     init_secure_boot();
     patch_network_vulnerability();
     optimize_power_management();
     init_sensor_array();
+    update_build_dependencies();
     // ... rest of firmware
 }
